4f929d5bf06e92a5d01c168571f63119e8627eb1
mausooq <mauchoomausooq051@gmail.com>
2024-02-05 22:41:49 +0530

    new

--- auth.js
+++ auth.js
@@ -8,8 +8,11 @@ let admins = [];
 let users = [];
 let courses = [];
 
+app.get('/admin/signup',(req, res) =>{
+    console.log("admin")
+})
 app.post('/admin/signup',(req, res) =>{
-    const admin =req.body
+    console.log("admin")
 })
 
-
+app.listen(3000, ()=> console.log("Server is running on port 3000"));
