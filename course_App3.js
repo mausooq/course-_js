c620166745fbdb66a82daa5e8d34fe3c51cfe0c4
mausooq <mauchoomausooq051@gmail.com>
2024-02-14 23:07:26 +0530

    new

--- course_App3.js
+++ course_App3.js
@@ -1,7 +1,16 @@
 // course selling app storing in mongodb
 const express = require('express')
+const mongoose = require('mongoose')
+const jwt = require('jsonwebtoken')
 const app = express();
 
 app.use(express.json());
+
+// define mongoose schema
+const userName = new mongoose.Schema({
+    userName : {typr : String},
+    Password : String,
+    purchasedCourse :["gata"]
+})
 
 app.listen(3000, () => console.log("Server is running on port 3000"));
